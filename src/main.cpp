35118027be9b2d9007cd332469c1cb3f06b622b8
pradhan-ritik <flipit001@gmail.com>
2026-09-17 18:29:17 -0700

    Drivetrain code works

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,8 @@ vex::brain       Brain;
 // define your global instances of motors and other devices here
 motor motorR1 = motor(PORT11, ratio6_1, false);
 motor motorR2 = motor(PORT12, ratio6_1, false);
-motor motorL1 = motor(PORT13, ratio6_1, false);
-motor motorL2 = motor(PORT14, ratio6_1, false);
+motor motorL1 = motor(PORT13, ratio6_1, true);
+motor motorL2 = motor(PORT14, ratio6_1, true);
 motor_group motorGroupR = motor_group(motorR1, motorR2);
 motor_group motorGroupL = motor_group(motorL1, motorL2);
 // Left Group, Right Group, Wheel Travel (in), Track Width (in), Wheel Base (in) units (in) gear ratio (36/60)
@@ -27,6 +27,7 @@ drivetrain drive(motorGroupL, motorGroupR, 8.639, 12.75, 8.5, distanceUnits::in,
 int main() {
 
     Brain.Screen.printAt( 10, 50, "Hello V5" );
+    drive.setDriveVelocity(100, percent);
    
     while(1) {
         drive.drive(forward);
