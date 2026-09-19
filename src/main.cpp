7c70d619de491e196379e9ee587285da1b1a9743
pradhan-ritik <flipit001@gmail.com>
2026-09-18 18:55:40 -0700

    code dr4b

--- src/main.cpp
+++ src/main.cpp
@@ -17,14 +17,17 @@ brain Brain;
 controller Controller1 = controller(primary);
 
 // Motor definitions (Reversed direction booleans)
-motor motorR1 = motor(PORT11, ratio6_1, true);
-motor motorR2 = motor(PORT12, ratio6_1, true);
-motor motorL1 = motor(PORT13, ratio6_1, false);
-motor motorL2 = motor(PORT14, ratio6_1, false);
+motor motorR1 = motor(PORT5, ratio6_1, true);
+motor motorR2 = motor(PORT20, ratio6_1, true);
+motor motorL1 = motor(PORT9, ratio6_1, false);
+motor motorL2 = motor(PORT6, ratio6_1, false);
+motor dr4bR = motor(PORT16, ratio36_1, false);
+motor dr4bL = motor(PORT17, ratio36_1, true);
 
 // Motor groups
 motor_group motorGroupR = motor_group(motorR1, motorR2);
 motor_group motorGroupL = motor_group(motorL1, motorL2);
+motor_group dr4b = motor_group(dr4bR, dr4bL);
 
 // Drivetrain setup
 drivetrain drive(motorGroupL, motorGroupR, 8.639, 12.75, 8.5, distanceUnits::in, 0.6);
@@ -34,6 +37,7 @@ int main() {
     motorGroupR.setStopping(coast);
 
     int deadband = 5;
+    int liftSpeed = 40;
 
     while (true) {
         // --- READ JOYSTICKS & SCALE TO 75% MAX SPEED ---
@@ -48,14 +52,14 @@ int main() {
         Controller1.Screen.print("Turn Vel: %d%%   ", turnSpeed);
 
         // Split-Arcade calculation
-        int leftVelocity  = forwardSpeed + turnSpeed;
-        int rightVelocity = forwardSpeed - turnSpeed;
+        int leftVelocity  = forwardSpeed - turnSpeed;
+        int rightVelocity = forwardSpeed + turnSpeed;
 
         // Clamp total combined motor velocity strictly between -75% and +75%
-        if (leftVelocity > 75)   leftVelocity = 75;
-        if (leftVelocity < -75)  leftVelocity = -75;
-        if (rightVelocity > 75)  rightVelocity = 75;
-        if (rightVelocity < -75) rightVelocity = -75;
+        if (leftVelocity < -75)   leftVelocity = -75;
+        if (leftVelocity > 75)  leftVelocity = 75;
+        if (rightVelocity < -75)  rightVelocity = -75;
+        if (rightVelocity > 75) rightVelocity = 75;
 
         // --- DRIVE LEFT MOTORS ---
         if (abs(leftVelocity) < deadband) {
@@ -71,6 +75,19 @@ int main() {
         } else {
             motorGroupR.setVelocity(rightVelocity, percent);
             motorGroupR.spin(forward);
+        }
+
+        
+        // dr4b
+        if (Controller1.ButtonR1.pressing()) {
+            dr4b.setVelocity(liftSpeed, percent);
+            dr4b.spin(forward);
+        } else if (Controller1.ButtonR2.pressing()) {
+            dr4b.setVelocity(liftSpeed, percent);
+            dr4b.spin(reverse);
+        } else {
+            // Stop and hold position against gravity
+            dr4b.stop();
         }
 
         // Brief delay to give time to the brain scheduler
